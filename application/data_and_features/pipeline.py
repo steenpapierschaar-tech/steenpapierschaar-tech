5652efd7794238198ec8bed303ee68e64e3fe52c
KRderksen <126659047+KRderksen@users.noreply.github.com>
2024-10-20 13:02:42 +0200

    werkende pipeline

--- application/data_and_features/pipeline.py
+++ application/data_and_features/pipeline.py
@@ -8,6 +8,11 @@ from dataset_loader import load_files
 import argparse
 
 def prepimg(image):
+    
+    
+
+   
+    
     img = cv.cvtColor(image,cv.COLOR_RGB2BGR)
     img[:,:,0] = 0
     img[:,:,1] = 0
@@ -17,24 +22,45 @@ def prepimg(image):
     #clahe = cv.createCLAHE(clipLimit=1.0, tileGridSize=(15,15))
     #gray  = clahe.apply(gray)
 
-    gray = cv.GaussianBlur(gray,(5,5),0)
+    gray = cv.GaussianBlur(gray,(9,9),0)
 
-    gray = cv.convertScaleAbs(gray, alpha=5.3 , beta=0)
-   
+    gray = cv.convertScaleAbs(gray, alpha=1.9, beta=0)
+    
     gray = extractpixels(gray) 
-    gray = cv.convertScaleAbs(gray, alpha=1.1 , beta=0)
     gray = extractpixels(gray)
-    gray = cv.convertScaleAbs(gray, alpha=1.1 , beta=0)
     gray = extractpixels(gray)
-    kernel=  np.ones((3,3),np.uint8)
-    gray = cv.dilate(gray,kernel,1)
+    gray = extractpixels(gray)
+    gray = extractpixels(gray)
+
+    gray = cv.convertScaleAbs(gray, alpha=2 , beta=0)
+    gray = extractpixels(gray)
+  
+    gray = extractpixels(gray)
+    gray = cv.convertScaleAbs(gray, alpha=2 , beta=0)
+    gray = extractpixels(gray)
+    gray = extractpixels(gray)
+    gray = extractpixels(gray)
+    gray = extractpixels(gray)
+    gray = extractpixels(gray)
+
+    gray = cv.GaussianBlur(gray,(5,5),0)
+    kernel=  np.ones((6,6),np.uint8)
+    canny = cv.Canny(gray,100,200)
+    canny = cv.dilate(canny,kernel,1)
+    
+    
+    gray[canny == 255]  = 0
+    gray = cv.convertScaleAbs(gray, alpha=1.3 , beta=0)
+    gray = cv.GaussianBlur(gray,(25,25),0)
+   # gray = extractpixels(gray)
     
     cv.imshow("gray",gray)
     
+    gray = cv.GaussianBlur(gray,(7,7),0)
     
-    #canny = cv.Canny(blurred,100,200)
-    _, thresh = cv.threshold(gray,175  , 255, cv.THRESH_BINARY_INV )
-    
+    cv.imshow("canny",canny)
+    _, thresh = cv.threshold(gray,128  , 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU )
+   # thresh = cv.dilate(thresh,kernel,1)
     cv.imshow("thres",thresh) 
 
 
@@ -42,15 +68,16 @@ def prepimg(image):
 
 def extractpixels(gray):
     #gray = cv.GaussianBlur(gray,(3,3),0)
-    noncappedpixels = gray [gray!= 255]
+    noncappedpixels = gray [gray!= 255 & (gray != 0)]
     if len(noncappedpixels) > 0:
         min_val = np.min(noncappedpixels)
+       
     
     
     output_image = np.copy(gray)
 
 # Subtract the minimum value from all non-255 pixels
-    output_image[output_image != 255] -= (min_val)
+    output_image[output_image != 255 &  (output_image>10)] -= (min_val)
 
 # Ensure pixel values don't go below 0
     output_image = np.clip(output_image, 0, 255)
