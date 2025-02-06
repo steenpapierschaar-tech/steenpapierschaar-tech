dd732b8c8b5992f519ac9660a70a6764fc08a483
jerwij <jltwijnands@gmail.com>
2025-02-06 19:54:39 +0100

    Changed parameters for DL layers
    accuracy: 0.9682 - loss: 0.1643 - val_accuracy: 0.8824 - val_loss: 0.3359

--- DL-application/application.py
+++ DL-application/application.py
@@ -11,59 +11,35 @@ from tensorflow.keras import layers, regularizers, optimizers
 
 def build_model():
     model = keras.Sequential()
-
     # Layer 0: Conv
-    model.add(layers.Conv2D(32, (3, 3), activation='gelu', padding='same',
-                            kernel_regularizer=regularizers.l2(0.0018481),
-                            input_shape=(config.IMAGE_ROWS,config.IMAGE_COLS, 3)))  # Adjust input shape as needed
+    model.add(layers.Conv2D(224, (7, 7), activation='leaky_relu', padding='same',
+                            kernel_regularizer=regularizers.l2(0.0001633),
+                            input_shape=(config.IMAGE_ROWS, config.IMAGE_COLS, 3)))  # Adjust input shape as needed
     model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(0.2))
+    model.add(layers.Dropout(0))
     model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-    # add optimizer amamw 
-    
 
     # Layer 1: Conv
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same',
-                            kernel_regularizer=regularizers.l2(0.0079166)))
+    model.add(layers.Conv2D(224, (7, 7), activation='leaky_relu', padding='same',
+                            kernel_regularizer=regularizers.l2(0.0001633)))
     model.add(layers.BatchNormalization())
-    model.add(layers.AveragePooling2D(pool_size=(4, 4)))
-
-
-
-    # Layer 2: Conv
-    model.add(layers.Conv2D(256, (3, 3), activation='leaky_relu', padding='same',
-                            kernel_regularizer=regularizers.l2(0.0058566)))
-    model.add(layers.Dropout(0.1))
     model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
-    # Layer 3: Conv
-    model.add(layers.Conv2D(256, (3, 3), activation='tanh', padding='same',
-                            kernel_regularizer=regularizers.l1(0.00015597)))
+    # Layer 2: Conv
+    model.add(layers.Conv2D(224, (7, 7), activation='leaky_relu', padding='same',
+                            kernel_regularizer=regularizers.l2(0.0001633)))
     model.add(layers.BatchNormalization())
-    model.add(layers.Dropout(0.4))
     model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
     # Flatten before Dense layers
     model.add(layers.Flatten())
 
     # Dense Layer 0
-    model.add(layers.Dense(32, activation='relu',
-                           kernel_regularizer=regularizers.l1(6.6062e-05)))
-    model.add(layers.Dropout(0.1))
-
-    # Dense Layer 1
-    model.add(layers.Dense(288, activation='leaky_relu',
-                           kernel_regularizer=regularizers.l1_l2(0.00010264)))
-    model.add(layers.Dropout(0.2))
-
-    # Dense Layer 2
-    model.add(layers.Dense(160, activation='leaky_relu',
-                           kernel_regularizer=regularizers.l1(0.00013522)))
-    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(96, activation='leaky_relu'))
     model.add(layers.Dropout(0.1))
 
     # Output layer (adjust units and activation for classification)
-    model.add(layers.Dense(3, activation='softmax'))  # Example: 10 classes for classification
+    model.add(layers.Dense(3, activation='softmax'))  # Example: 3 classes for classification
 
     return model
 
